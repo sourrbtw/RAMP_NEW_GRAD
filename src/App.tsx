3c8c3e46ca7e22e2624b8883ab3544cd70254037
sourrbtw <rekwenib@iu.edu>
2023-09-01 22:58:41 -0400

    Bug 6 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ export function App() {
         <div className="RampGrid">
           <Transactions transactions={transactions} />
 
-          {transactions !== null && !isFilteredByEmployee && (
+          {transactions !== null && !isFilteredByEmployee && paginatedTransactions?.nextPage !== null && (
             <button
               className="RampButton"
               disabled={paginatedTransactionsUtils.loading}
